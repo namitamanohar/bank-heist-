35d82c935e6a1c62f5a31b13c7a0556e82c3831f
namitamanohar <manohar.namita@gmail.com>
2020-02-28 14:23:24 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace bankHeist {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Plan your heist");
+            Console.WriteLine ("Enter a team member's name:");
+            string memberName = Console.ReadLine ();
+
+            Console.WriteLine ("Enter a team member's skill level:");
+            string skillLevel = Console.ReadLine ();
+
+            Console.WriteLine ("Enter a team member's courage factor:");
+            string courageFactor = Console.ReadLine ();
+
+            Console.WriteLine ($"Team member name : {memberName} Skill Level {skillLevel} Courage level: {courageFactor} ");
+        }
+    }
+}
