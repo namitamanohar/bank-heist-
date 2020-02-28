07be83ffce3eb36ea45e47cb30b40b0727a53098
namitamanohar <manohar.namita@gmail.com>
2020-02-28 15:48:06 -0600

    phase 1 and 2 working on 3

--- Program.cs
+++ Program.cs
@@ -1,19 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace bankHeist {
     class Program {
         static void Main (string[] args) {
+            // dictionary for each team member 
+
+            List<Dictionary<string, string>> AllTeamInformation = new List<Dictionary<string, string>> ();
+
+            Dictionary<string, int> bankDifficultyLevel = new Dictionary<string, int> ();
+            bankDifficultyLevel.Add ("difficulty", 100);
+
             Console.WriteLine ("Plan your heist");
-            Console.WriteLine ("Enter a team member's name:");
-            string memberName = Console.ReadLine ();
+            while (true) {
 
-            Console.WriteLine ("Enter a team member's skill level:");
-            string skillLevel = Console.ReadLine ();
+                Console.WriteLine ("Enter a team member's name:");
+                Dictionary<string, string> teamMemberInfo = new Dictionary<string, string> ();
+                string memberName = Console.ReadLine ();
+                teamMemberInfo.Add ("Member Name", memberName);
 
-            Console.WriteLine ("Enter a team member's courage factor:");
-            string courageFactor = Console.ReadLine ();
+                if (memberName != "") {
+                    Console.WriteLine ("Enter a team member's skill level:");
+                    teamMemberInfo.Add ("Skill Level", Console.ReadLine ());
 
-            Console.WriteLine ($"Team member name : {memberName} Skill Level {skillLevel} Courage level: {courageFactor} ");
+                    Console.WriteLine ("Enter a team member's courage factor:");
+                    teamMemberInfo.Add ("Courage Level", Console.ReadLine ());
+
+                    AllTeamInformation.Add (teamMemberInfo);
+
+                } else {
+                    break;
+                }
+            }
+            foreach (Dictionary<string, string> teamMember in AllTeamInformation)
+                foreach (KeyValuePair<string, string> item in teamMember) {
+                    // Console.WriteLine ($"{item.Key}: {item.Value}");
+
+                }
+            Console.WriteLine ($"You  have {AllTeamInformation.Count} on your team");
         }
     }
 }
